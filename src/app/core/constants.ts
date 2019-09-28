b29283ae24c55c0173b818b8236d234258c7f422
Miroslav Jonas <missing.manual@gmail.com>
2019-09-28 20:22:20 +0200

    docs: add sizes

---
+++ src/app/core/constants.ts
@@ -0,0 +1,11 @@
+// Meetup
+// Group Banner image dimensions: 600 x 338
+// Event Page Featured Photo: 592 x 342
+
+// Twitter
+// Post image dimensions: 1024 x 512.
+
+// Facebook
+// Event header image dimensions: 1200 x 628.
+// Profile post image dimensions: 1200 x 628.
+// Page Post image dimensions: 1200 x 900.
