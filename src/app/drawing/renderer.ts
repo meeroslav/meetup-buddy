716f8a6a21f67bc638c6d620fab05b7566d60b37
Miroslav Jonas <missing.manual@gmail.com>
2019-10-15 13:19:09 +0200

    feat: resize background to fit canvas

--- src/app/drawing/renderer.ts
+++ src/app/drawing/renderer.ts
@@ -5,8 +5,6 @@ import { EventMetadata } from '../model/event-metadata';
 export function render(canvas: HTMLCanvasElement, width: number, height: number, model: EventMetadata): HTMLCanvasElement {
   console.log(dateFormat(model.date, DATE_FORMAT));
 
-  console.log(width, height);
-
   canvas.height = height;
   canvas.width = width;
 
@@ -18,7 +16,15 @@ export function render(canvas: HTMLCanvasElement, width: number, height: number,
 function drawBackground(ctx: CanvasRenderingContext2D, imageUrl: string) {
   const image = new Image();
   image.onload = () => {
-    ctx.drawImage(image, 0, 0);
+    const canvasRatio = ctx.canvas.width / ctx.canvas.height;
+    const imageRatio = image.width / image.height;
+    if (imageRatio > canvasRatio) {
+      const delta = imageRatio * ctx.canvas.height - ctx.canvas.width;
+      ctx.drawImage(image, delta / -2, 0, ctx.canvas.width + delta / 2, ctx.canvas.height);
+    } else {
+      const delta = imageRatio * ctx.canvas.width - ctx.canvas.height;
+      ctx.drawImage(image, 0, delta / -2, ctx.canvas.width, ctx.canvas.height + delta / 2);
+    }
   };
   image.src = imageUrl;
 }
